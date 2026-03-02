b95d4fc9d6295b07dae74ae60ab9476ab735ab5e
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-03-02 19:55:43 -0300

    Iniciado

---
+++ codigo.py
@@ -0,0 +1,12 @@
+# Passo 1: Entrar no sistema da Empresa
+# Passo 2: Fazer Login
+# Passo 3: Abrir a base de dados
+# Passo 4: Cadastrar um produto
+# Passo 5: Repetir o passo 4 até a lista de produtos acabar
+
+# pip install pyautogui
+
+
+import pyautogui
+
+pyautogui.clic
