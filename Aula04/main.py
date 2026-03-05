413b915b5ac8860702b62c75becc99072b8b7e65
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-03-05 19:47:01 -0300

    Aula 04 iniciada

---
+++ Aula04/main.py
@@ -0,0 +1,15 @@
+# Passo a passo
+
+# Input do chat
+    # Mostrar a mensagem que o usuario enviou
+    # Pegar a pergunta e enviar para a IA
+    # Exibir a resposta na IA na tela
+
+# Usaremos Frameworks, que é um pacote de código com regras a serem seguidas.
+# Algum dos principais são: Streamlit, Flask, Django, FastAPI (Usaremos Streamlit)
+
+# No Streamlit, podemos apenas com Python criar o frontend e o backend
+# a IA que será usada é: OpenAI (ChatGPT)
+# pip install openai streamlit
+
+import streamlit as st
