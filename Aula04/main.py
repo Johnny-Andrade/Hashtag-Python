b25f771a74f101a09e608e5a17b113a1d355dae1
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-03-05 21:07:55 -0300

    Progresso na aula

--- Aula04/main.py
+++ Aula04/main.py
@@ -14,6 +14,32 @@
 # pip install openai streamlit
 
 import streamlit as st
+from openai import OpenAI
+
+modelo_ia = OpenAI(api_key="")
+
+if not "lista_mensagens" in st.session_state:
+    st.session_state["lista_mensagens"] = []
 
 st.write("Danonebot com IA")
-st.write("Teste de Python")
+
+texto_usuario = st.chat_input("Digite sua mensagem...")
+
+for mensagem in st.session_state["lista_mensagens"]:
+    role = mensagem["role"]
+    content = mensagem["content"]
+    st.chat_message(role).write(content) 
+
+if texto_usuario: #Se houver algum texto do usuario (para nao ficar enviando None)
+    st.chat_message("user").write(texto_usuario) 
+    #pode ser um nome, user(ícone de pessoal), assistant (ícone da IA)
+    mensagem_usuario = {"role": "user", "content": texto_usuario}
+    st.session_state["lista_mensagens"].append(mensagem_usuario)
+
+    resposta_ia = "Você me perguntou: " + texto_usuario
+
+    st.chat_message("assistant").write(resposta_ia)
+    mensagem_ia = {"role": "assistant", "content": resposta_ia}
+    st.session_state["lista_mensagens"].append(mensagem_ia)
+
+print(st.session_state["lista_mensagens"])
